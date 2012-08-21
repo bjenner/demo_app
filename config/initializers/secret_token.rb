157c687362cff3aa384e92471a5176f30bee0bdd
Bruce Jenner <Bruce.Jenner@gmail.com>
2012-08-20 17:46:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8a3439e338e55186178e0d22588257b8896ddb88801a36891a1db0491c9a2fc6f7a90208d2659796a8914c7b1e69e85bef33ae649e42704991ecd9f88411048c'
